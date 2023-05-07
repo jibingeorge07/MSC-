185cb3d988a750def093989c8d76c28d29072037
Maria Thomas <mariathomas310212@gmail.com>
2023-05-07 10:19:13 +0100

    add admin login details

--- app/app.js
+++ app/app.js
@@ -4,8 +4,14 @@ const express = require("express");
 // Create express app
 var app = express();
 
+//body parser
+const bodyParser = require('body-parser');
+
 // Add static files location
 app.use(express.static("static"));
+
+//Add body parser files location
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
@@ -17,6 +23,15 @@ app.set('views', './app/views');
 // Get the models
 const { User } = require("./models/user");
 
+// Set the sessions
+var session = require('express-session');
+app.use(session({
+  secret: 'secretkeysdfjsflyoifasd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 // Create a route for root - /
 app.get("/", function(req, res) {
     // Set up an array of data
@@ -24,31 +39,80 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
+//admin page 
+app.get('/admin', function(req, res) {
+    res.render('login');
+  });
+
+// Define the authenticateUser function
+function authenticateUser(email, password) {
+    const query = 'SELECT * FROM users WHERE email=? AND password=?';
+    const values = [email, password];
+    return db.query(query, values).then(([rows, fields]) => {
+      if (!rows || rows.length === 0) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+  
+  // Define the login route handler
+app.post('/login', function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    // Call the authenticateUser function
+    authenticateUser(email, password).then((authenticated) => {
+      if (authenticated) {
+      // Set the authenticated session variable
+        req.session.authenticated = true;
+        res.redirect('/user');
+      } else {
+        res.render('error', { error: 'Invalid email or password' });
+      }
+    }).catch((err) => {
+        console.log("error")
+    });
+});
+
+app.get('/user', function(req, res) {
+    if (req.session.authenticated) {
+      var sql = 'select * from UserProfile';
+      db.query(sql).then(results => {
+        res.render('all_users', {data: results});
+      }).catch((err) => {
+        console.error(err);
+        res.render('error', { error: 'Internal server error' });
+      });
+    } else {
+      res.redirect('/admin');
+    }
+  });
+
+// Logout
+app.post('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/admin');
+});
+
 app.get("/RSVP", function(req, res) {
     res.render("RSVP");
 });
 
-// Task 2 display a formatted list of students
-app.get("/admin", function(req, res) {
-    var sql = 'select * from UserProfile';
-    db.query(sql).then(results => {
-    	    // The rows will be in a variable called data
-        res.render('all_users', {data: results});
-    });
-});
-
-// Single student page, using MVC pattern
 app.get("/single_user/:id", async function (req, res) {
-    var stId = req.params.id;
-    var user = new User(stId);
-    await user.getUserDetails();
-    await user.getCakeName();
-    await user.getVenuesName();
-    await user.getDecorationName();
-    await user.getFlowerName();
-    res.render('user', {user:user});
-});
-
+    if (req.session.authenticated) {
+        var stId = req.params.id;
+        var user = new User(stId);
+        await user.getUserDetails();
+        await user.getCakeName();
+        await user.getVenuesName();
+        await user.getDecorationName();
+        await user.getFlowerName();
+        res.render('user', {user:user});
+    } else {
+      res.redirect('/admin');
+    }
+  });
 
 // Create a route for testing the db
 app.get("/db_test", function(req, res) {
